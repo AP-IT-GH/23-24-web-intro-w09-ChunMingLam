6323d1e6bfdc4e67f764f2072e35b2ac5b2f1a54
ChunMingLam <99297467+ChunMingLam@users.noreply.github.com>
2024-04-28 15:50:54 +0200

    labo opdracht 9
    kaart in contact

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -1,17 +1,15 @@
-/* In dit script plaats je de code om de kaart in de id apMap te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat.  
-* Gebruik hiervoor de documentatie op https://leafletjs.com/ 
-* Verander in de code online alle "var" in "let".
-*/
+/* In dit script plaats je de code om de kaart in de id apMap te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat.
+ * Gebruik hiervoor de documentatie op https://leafletjs.com/
+ * Verander in de code online alle "var" in "let".
+ */
 
+let map = L.map("apMap").setView([51.23009, 4.41616], 17);
 
-
-
-
-
-
-
-
-
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
 
 // bepaal de rechthoek rondom het gebouw van AP
 
